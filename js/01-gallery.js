060c80aa856ddc3ade5d119c3f0066ecac3bc43d
Ivaskiv <viraivaskiv@gmail.com>
2023-10-24 17:07:20 +0300

    01-gallery.js update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,78 +1,138 @@
-//! Завдання 1 - галерея зображень
-// Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
-
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-//1. виберіть контейнер <ul class="gallery">, до якого ви хочете додати розмітку - отримати посилання на елемент ul.gallery:
-const gallery = document.querySelector('.gallery');
-// функція для створення розмітки
-function createGalleryMarkup(items) {
-    return items
-        .map(({ preview, original, description }) => {
-            return `
-            <li class="gallery__item">
-                <a class="gallery__link" href="${original}">
-                    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
-                </a>
-            </li>
-            `;
-        })
-        .join('');
+//! 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+const galleryElement = document.querySelector(".gallery");
+//функція буде приймати масив і створювати HTML-розмітку для кожного елемента галереї
+function createGallery(img) {
+  return img
+    .map(({ preview, original, description }) => {
+      return `
+        <li class='gallery__item'>
+          <a class='gallery__link' href='${original}'>
+            <img 
+              class='gallery__image'
+              src='${preview}'
+              data-source='${original}'
+              alt='${description}'
+            />
+          </a>
+        </li>
+    `;
+    })
+    .join("");
 }
-// Реалізація делегування на ul.gallery і отримання url великого зображення
-function onGalleryItemClick(event) {
- // У цій функції ми перевіряємо, чи клікнули на зображенні, якщо так, отримуємо URL великого зображення з атрибуту data-source
- event.preventDefault();
- if (event.target.nodeName !== 'IMG') {
-  return;
- }
-// Отримай URL великого зображення
-    const largeImageUrl = event.target.dataset.source;
-    // Тепер ви можете використовувати цей URL для відкриття модального вікна
-    openModal(largeImageUrl);
-}
+galleryElement.insertAdjacentHTML("beforeend", createGallery(galleryItems));
+//!2. Реалізація делегування на ul.gallery і отримання url великого зображення.
+galleryElement.addEventListener("click", (event) => {
+  event.preventDefault();
+  //перевірка чи клікнуто на зображенні:
+  if (event.target.tagName !== "IMG") {
+    return;
+  }
+  // Якщо так, то отримати URL великого зображення з використанням basicLightbox
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width='800' height='600'>`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", onEsc); //modal open
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", onEsc); //modal close
+      },
+    }
+  );
+  function onEsc(e) {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  }
+  instance.show();
+});
+// ****************II********************** //
 
+// const galleryElements = document.querySelector(".gallery");
+// // const galleryCreate = makeGallery(galleryItems);
+// galleryElements.insertAdjacentHTML("beforeend", galleryCreate);
+// galleryElements.addEventListener("click", openGalleryModal);
 
+// const imageInstance = basicLightbox.create(document.querySelector("#image"));
+// document.querySelector("button.image").onclick = imageInstance.show;
 
+// //create elements HTML
+// // import * as basicLightbox from "basiclightbox";
 
+// const galleryCreate = basicLightbox.create(`
+// <a class="gallery__item" href=${original}>
+//         <img class="gallery__image" src=${preview} alt=${description} />
+//       </a>
+// `);
 
+// instance.show();
+// // function makeGallery(galleryItems) {
+// //   return galleryItems
+// //     .map(({ preview, original, description }) => {
+// //       return `
+// //       <a class="gallery__item" href=${original}>
+// //         <img class="gallery__image" src=${preview} alt=${description} />
+// //       </a>
+// //     `;
+// //     })
+// //     .join("");
+// // }
+// function openGalleryModal(event) {
+//   event.preventDefault();
+//   if (event.target.nodeName !== "img") {
+//     return;
+//   }
+//   instance.element().querySelector("img").src = event.target.dataset.source;
+// }
+// instance.show();
+// //create el. gallery
+// const instance = basicLightbox.create(`<img src=''>`, {
+//   onShow: () => {
+//     document.addEventListener("keydown", onEsc); //modal open
+//   },
+//   onClose: () => {
+//     document.removeEventListener("keydown", onEsc); //modal close
+//   },
+// });
+// function onEsc(e) {
+//   if (e.code === "Escape") {
+//     instance.close();
+//   }
+// }
+// ****************I********************** //
+// // Формуємо розмітку галереї на основі масиву даних
+// const createGallery = (galleryItems) => {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `
+//             <a class="gallery__item" href=${original}>
+//                 <img class="gallery__image" src=${preview} alt=${description} />
+//             </a>`;
+//     })
+//     .join("");
+// };
 
+// // Рендеримо розмітку галереї
+// galleryElements.insertAdjacentHTML("beforeend", createGallery(galleryItems));
 
+// const lightbox = new basicLightbox(".gallery a", {
+//   overlayOpacity: 0.5,
+//   captionDelay: 250,
+//   captionsData: "alt",
+//   fadeSpeed: 250,
+//   rtl: true,
+// });
 
- gallery.addEventListener('click', onGalleryItemClick);
-
-console.log(galleryItems);
-
-
-
-
-
-// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
-
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-// Реалізація делегування на ul.gallery і отримання url великого зображення.
-// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
-// Розмітка елемента галереї
-// Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>, і вказуватися в href посилання. Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
-
-// <li class="gallery__item">
-//   <a class="gallery__link" href="large-image.jpg">
-//     <img
-//       class="gallery__image"
-//       src="small-image.jpg"
-//       data-source="large-image.jpg"
-//       alt="Image description"
-//     />
-//   </a>
-// </li>
-
-// Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. Заборони цю поведінку за замовчуванням.
-
-// Закриття з клавіатури
-// УВАГА
-// Наступний функціонал не обов'язковий для здавання завдання, але буде хорошою додатковою практикою.
-
-// Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. Бібліотека basicLightbox містить метод для програмного закриття модального вікна.
+// //делегування подій
+// galleryElements.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   if (event.target.tagName === "img") {
+//     const largeImageURL = event.target.dataset.source; //отримати url
+//     console.log(largeImageURL);
+//     const instance = basicLightbox.create(`
+//     <img src='${largeImageURL}' alt='Image description'> width="800" height="600"`);
+//     instance.show();
+//   }
+// });
