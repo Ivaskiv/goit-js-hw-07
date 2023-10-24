1e7ba34a35686b4a21252d1ced3d54a7a8240fd4
Ivaskiv <viraivaskiv@gmail.com>
2023-10-24 17:26:04 +0300

    add link to html

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -30,9 +30,12 @@ galleryElement.addEventListener("click", (event) => {
     return;
   }
   // Якщо так, то отримати URL великого зображення з використанням basicLightbox
+  // Цей код створює модальне вікно з зображенням та встановлює велике зображення, вказане в атрибуті data-source, на яке клікнули.
+  //! 5. Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
   const instance = basicLightbox.create(
     `<img src="${event.target.dataset.source}" width='800' height='600'>`,
     {
+      //!4. Відкриття модального вікна по кліку на елементі галереї.
       onShow: () => {
         document.addEventListener("keydown", onEsc); //modal open
       },
@@ -48,91 +51,5 @@ galleryElement.addEventListener("click", (event) => {
   }
   instance.show();
 });
-// ****************II********************** //
 
-// const galleryElements = document.querySelector(".gallery");
-// // const galleryCreate = makeGallery(galleryItems);
-// galleryElements.insertAdjacentHTML("beforeend", galleryCreate);
-// galleryElements.addEventListener("click", openGalleryModal);
-
-// const imageInstance = basicLightbox.create(document.querySelector("#image"));
-// document.querySelector("button.image").onclick = imageInstance.show;
-
-// //create elements HTML
-// // import * as basicLightbox from "basiclightbox";
-
-// const galleryCreate = basicLightbox.create(`
-// <a class="gallery__item" href=${original}>
-//         <img class="gallery__image" src=${preview} alt=${description} />
-//       </a>
-// `);
-
-// instance.show();
-// // function makeGallery(galleryItems) {
-// //   return galleryItems
-// //     .map(({ preview, original, description }) => {
-// //       return `
-// //       <a class="gallery__item" href=${original}>
-// //         <img class="gallery__image" src=${preview} alt=${description} />
-// //       </a>
-// //     `;
-// //     })
-// //     .join("");
-// // }
-// function openGalleryModal(event) {
-//   event.preventDefault();
-//   if (event.target.nodeName !== "img") {
-//     return;
-//   }
-//   instance.element().querySelector("img").src = event.target.dataset.source;
-// }
-// instance.show();
-// //create el. gallery
-// const instance = basicLightbox.create(`<img src=''>`, {
-//   onShow: () => {
-//     document.addEventListener("keydown", onEsc); //modal open
-//   },
-//   onClose: () => {
-//     document.removeEventListener("keydown", onEsc); //modal close
-//   },
-// });
-// function onEsc(e) {
-//   if (e.code === "Escape") {
-//     instance.close();
-//   }
-// }
-// ****************I********************** //
-// // Формуємо розмітку галереї на основі масиву даних
-// const createGallery = (galleryItems) => {
-//   return galleryItems
-//     .map(({ preview, original, description }) => {
-//       return `
-//             <a class="gallery__item" href=${original}>
-//                 <img class="gallery__image" src=${preview} alt=${description} />
-//             </a>`;
-//     })
-//     .join("");
-// };
-
-// // Рендеримо розмітку галереї
-// galleryElements.insertAdjacentHTML("beforeend", createGallery(galleryItems));
-
-// const lightbox = new basicLightbox(".gallery a", {
-//   overlayOpacity: 0.5,
-//   captionDelay: 250,
-//   captionsData: "alt",
-//   fadeSpeed: 250,
-//   rtl: true,
-// });
-
-// //делегування подій
-// galleryElements.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   if (event.target.tagName === "img") {
-//     const largeImageURL = event.target.dataset.source; //отримати url
-//     console.log(largeImageURL);
-//     const instance = basicLightbox.create(`
-//     <img src='${largeImageURL}' alt='Image description'> width="800" height="600"`);
-//     instance.show();
-//   }
-// });
+//! 3. Підключення скрипту і стилів бібліотеки модального вікна basicLightbox =>в HTML!!!(8, 20 РЯДОК) Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
